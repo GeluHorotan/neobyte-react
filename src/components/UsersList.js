46f9460f8e4f28f5c147e0c4aad58446f2adfef0
Horotan Gelu <horotangelu17@gmail.com>
2022-09-03 18:52:49 +0300

    Users list component has been created

---
+++ src/components/UsersList.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router';
+import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
+
+const UsersList = () => {
+  const [data, setData] = useState();
+  const [dataCopy, setDataCopy] = useState();
+  const [searchParams, setSearchParam] = useSearchParams();
+  const searchTerm = searchParams.get('name') || '';
+  const fetchUsers = async () => {
+    const res = await fetch(`/.netlify/functions/getUsers/`);
+    const json = await res.json();
+
+    setData(() => json);
+    setDataCopy(() => json);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const searchHandler = (event) => {
+    const name = event.target.value;
+    if (name) {
+      setSearchParam({ name });
+    } else {
+      setSearchParam({});
+    }
+  };
+
+  const filterUsers = (dataSet) => {
+    const filteredData = dataSet.filter((user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return filteredData;
+  };
+  return (
+    <WrapperStyles>
+      <input
+        style={{ marginTop: '10rem' }}
+        type='text'
+        onChange={searchHandler}
+      />
+      <UsersListStyles>
+        {dataCopy &&
+          filterUsers(dataCopy).map((user, index) => {
+            return <div key={index}>{user.name}</div>;
+          })}
+      </UsersListStyles>
+    </WrapperStyles>
+  );
+};
+
+const WrapperStyles = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const UsersListStyles = styled.div`
+  width: 100%;
+  background: red;
+  display: flex;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export default UsersList;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,58 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { useLocation } from 'react-router';
-import { useSearchParams } from 'react-router-dom';
+import UsersList from '../components/UsersList';
 
 const Homepage = () => {
-  const [data, setData] = useState();
-  const [dataCopy, setDataCopy] = useState();
-  const [searchParams, setSearchParam] = useSearchParams();
-  const searchTerm = searchParams.get('name') || '';
-  const fetchUsers = async () => {
-    const res = await fetch(`/.netlify/functions/getUsers/`);
-    const json = await res.json();
-
-    setData(() => json);
-    setDataCopy(() => json);
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
-  const searchHandler = (event) => {
-    const name = event.target.value;
-    if (name) {
-      setSearchParam({ name });
-    } else {
-      setSearchParam({});
-    }
-  };
-
   return (
     <HomepageStyles>
-      <input
-        style={{ marginTop: '10rem' }}
-        type='text'
-        onChange={searchHandler}
-      />
-
-      <h1>NEOBYTE</h1>
-      <h2>NEOBYTE</h2>
-      <h3>NEOBYTE</h3>
-      <h4>NEOBYTE</h4>
-      <h5>NEOBYTE</h5>
-      <h6>NEOBYTE</h6>
-      <p>NEOBYTE</p>
-
-      {/* {dataCopy &&
-        dataCopy
-          .filter((user) =>
-            user.name.toLowerCase().includes(searchTerm.toLowerCase())
-          )
-          .map((user, index) => {
-            return <div key={index}>{user.name}</div>;
-          })} */}
+      <UsersList></UsersList>
     </HomepageStyles>
   );
 };
