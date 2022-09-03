f75e743e606c1b14c17855cf30c71bed858799a7
Horotan Gelu <horotangelu17@gmail.com>
2022-09-04 01:06:46 +0300

    Micro header refactored

--- src/components/MicroHeader.js
+++ src/components/MicroHeader.js
@@ -1,67 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 
 const MicroHeader = () => {
   const navigate = useNavigate();
-
   return (
     <MicroHeaderStyles>
       <NeobyteLogoStyles
         src='https://www.neobytesolutions.com/wp-content/uploads/2022/04/logo_Neobyte_solutions-Bold.webp'
         alt='neobyte_logo'
+        onClick={() => {
+          navigate('/');
+        }}
       />
-      <TextStyles>
-        <h6
-          onClick={() => {
-            navigate('/');
-          }}
-        >
-          HOME
-        </h6>
-        <h6
-          onClick={() => {
-            navigate('/test');
-          }}
-        >
-          Project Details
-        </h6>
-      </TextStyles>
     </MicroHeaderStyles>
   );
 };
 
 const MicroHeaderStyles = styled.section`
   width: 100%;
-
   padding: 2rem;
-  background-color: #131338;
   display: flex;
+  position: fixed;
+  background: transparent;
+  justify-content: center;
   align-items: center;
-`;
-
-const TextStyles = styled.div`
-  width: 100%;
-
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
-  padding: 0 24vw;
-  h6 {
-    text-decoration: none;
-    text-transform: uppercase;
-    color: #fff;
-    cursor: pointer;
-  }
+  z-index: 10;
 `;
 
 const NeobyteLogoStyles = styled.img`
   width: 10%;
   transition: all 200ms ease-in-out;
   cursor: pointer;
-  position: absolute;
-  left: 5%;
+
   &:hover {
     transform: scale(1.1);
   }
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import SearchIllustration from '../components/SearchIllustration';
+
 import UsersList from '../components/UsersList';
 
 const Homepage = () => {
